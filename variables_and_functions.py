0de6c1877e9b93032caf160860a48eb552508c1b
Jared Flomen <jaredflomen@gmail.com>
2021-01-29 11:37:22 -0500

    Create variables_and_functions.py
    Exploring variables and functions

---
+++ variables_and_functions.py
@@ -0,0 +1,24 @@
+first_name = 'Jared'
+last_name = 'Flomen'
+country = 'Canada'
+city = 'Toronto'
+age = 26
+is_married = False
+skills = ['React', 'Ruby on Rails', 'JS']
+person_info = {
+  'firstname': 'Jared',
+  'lastname': 'Flomen',
+  'country': 'Canada',
+  'city': 'Toronto'
+}
+
+print('First name:', first_name)
+print('First name length:', len(first_name))
+print('Last name: ', last_name)
+print('Last name length: ', len(last_name))
+print('Country: ', country)
+print('City: ', city)
+print('Age: ', age)
+print('Married: ', is_married)
+print('Skills: ', skills)
+print('Person information: ', person_info)
