944edd9dfd8e524d2d9657cd6b1efbd056d3b751
Jared Flomen <jaredflomen@gmail.com>
2021-01-30 12:22:55 -0500

    Update operators.py
    Area of rectangle, weight of an object

--- operators.py
+++ operators.py
@@ -28,3 +28,15 @@ print('a ** b = ', exponential)
 radius = 10
 area_of_circle = 3.14 * radius ** 2
 print('Area of circle: ', area_of_circle)
+
+# Calculating area of a rectangle
+length = 10
+width = 20
+area_of_rectangle = length * width
+print('Area of rectangle:', area_of_rectangle)
+
+# Calculating a weight of an object
+mass = 75
+gravity = 9.81
+weight = mass * gravity
+print(weight, 'N')
