1b880ce026f20cd4c0063ce9421ee9dfb999fd31
Jared Flomen <jaredflomen@gmail.com>
2021-01-30 12:20:47 -0500

    Create operators.py
    Operators

---
+++ operators.py
@@ -0,0 +1,25 @@
+print('Addition: ', 1 + 2)
+print('Division without the remainder: ', 7 // 2)
+print('Moduus: ', 3 % 2)
+print('Division without the remainder: ', 7 // 3)
+print('Exponential: ', 3 ** 2)
+
+a = 3
+b = 2
+
+total = a + b
+diff = a - b
+product = a * b
+division = a / b
+remainder = a % b
+floor_division = a // b
+exponential = a ** b
+
+print(total)
+print('a + b = ', total)
+print('a - b = ', diff)
+print('a * b = ', product)
+print('a / b = ', division)
+print('a % b = ', remainder)
+print('a // b = ', floor_division)
+print('a ** b = ', exponential)
