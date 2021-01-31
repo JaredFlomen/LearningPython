4a70cf41d1e644f776fd090b49db822f69aeb4ac
Jared Flomen <jaredflomen@gmail.com>
2021-01-31 13:15:21 -0500

    Update strings.py
    New string style format

--- strings.py
+++ strings.py
@@ -13,3 +13,7 @@ area = pi * radius ** 2
 formatted_string = 'The area of a circle with a radius %d is %.2f.' % (radius, area)
 #2 refers to 2 decimals after the float
 print(formatted_string)
+
+#New Formatting
+formatted_string = 'I am {} {}. I\'m learning {}'.format(first_name, last_name, language)
+print(formatted_string)
