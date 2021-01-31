82d1f231ad5f08ec7bc071bf22acf3006e8fe0db
Jared Flomen <jaredflomen@gmail.com>
2021-01-31 14:03:13 -0500

    Create lists.py
    Unpacking lists

---
+++ lists.py
@@ -0,0 +1,8 @@
+#Lists
+
+lst = ['Leafs', 'Habs', 'Sens', 'Oilers', 'Flames']
+first_item, second_item, third_item, *rest = lst
+print(first_item)
+print(second_item)
+print(third_item)
+print(rest)
