df862d466c9fc803f2443fc928b2b3527b796fd9
Jared Flomen <jaredflomen@gmail.com>
2021-01-31 13:13:46 -0500

    Update strings.py
    Formatting numbers

--- strings.py
+++ strings.py
@@ -6,3 +6,10 @@ last_name = 'Flomen'
 language = 'Python'
 formatted_string = 'I am %s %s. I\'m learning %s' % (first_name, last_name, language)
 print(formatted_string)
+
+radius = 10
+pi = 3.14
+area = pi * radius ** 2
+formatted_string = 'The area of a circle with a radius %d is %.2f.' % (radius, area)
+#2 refers to 2 decimals after the float
+print(formatted_string)
