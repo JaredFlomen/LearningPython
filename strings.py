72709bd702d50e93bf30d7c7cdf82aadf749bfd7
Jared Flomen <jaredflomen@gmail.com>
2021-01-31 13:12:09 -0500

    Update strings.py
    Strings formatting

--- strings.py
+++ strings.py
@@ -1,1 +1,8 @@
 #Day 4
+#\n \t \\ \' \"
+
+first_name = 'Jared'
+last_name = 'Flomen'
+language = 'Python'
+formatted_string = 'I am %s %s. I\'m learning %s' % (first_name, last_name, language)
+print(formatted_string)
