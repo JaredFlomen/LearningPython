4bacdd2eba8b51299a2c83c9c09506720a555912
Jared Flomen <jaredflomen@gmail.com>
2021-02-01 11:32:33 -0500

    Update loops.py
    Looping

--- loops.py
+++ loops.py
@@ -1,8 +1,22 @@
 count = 0
 while count < 5:
-  print(count)
-  count = count + 1
   if count == 3:
     break
-else:
   print(count)
+  count = count + 1
+print('end')
+
+numbers = [0, 1, 2, 3, 4]
+for number in numbers:
+  print(number)
+
+person = {
+  'first_name': 'Jared',
+  'last_name': 'Flomen',
+  'age': 25,
+  'country': 'Canada',
+  'skills': ['Javascript', 'React', 'Python', 'Ruby']
+}
+
+for key, value in person.items():
+  print(key, value)
