fdf686ab87b91a9b611f2edd64093d221f2baf86
Jared Flomen <jaredflomen@gmail.com>
2021-02-01 10:21:17 -0500

    Update tuples.py
    Exercises with tuples

--- tuples.py
+++ tuples.py
@@ -3,3 +3,16 @@
 cities = ('Toronto', 'Montreal', 'Vancouver', 'Halifax')
 cities = list(cities)
 print(cities)
+print('Hamilton' in cities)
+
+#Examples
+empty_tuple = ()
+brothers = ('Adam', 'Kobe')
+sisters = ('Sam', 'Jaclyn')
+siblings = brothers + sisters
+print(siblings)
+print(len(siblings))
+family = siblings + ('Richard', 'Lisa')
+print(family)
+print('Kobe' in family)
+print('Bingo' in family)
