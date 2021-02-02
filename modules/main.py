e0e943a5a1b886880f6ebe3ac930f490ed613be8
Jared Flomen <jaredflomen@gmail.com>
2021-02-02 09:33:43 -0500

    Update main.py
    Sys module

--- modules/main.py
+++ modules/main.py
@@ -2,5 +2,10 @@ from mymodules import generate_full_name, sum_two_num as sum
 print(generate_full_name('Jared', 'Flomen'))
 print(sum(1, 2))
 
-import os
-os.mkdir('test_directory')
+#Os module
+# import os
+# os.mkdir('test_directory')
+
+#Sys module
+import sys
+print('Welcome {}. Enjoy {}'.format(sys.argv[1], sys.argv[2]))
