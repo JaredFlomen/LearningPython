39c25ee4ff6d3b862c98f70295e2cec2fedd903a
Jared Flomen <jaredflomen@gmail.com>
2021-02-02 08:57:58 -0500

    Create functions.py
    Functions returning a value

---
+++ functions.py
@@ -0,0 +1,16 @@
+def generate_name ():
+    first_name = 'Jared'
+    last_name = 'Flomen'
+    space = ' '
+    full_name = first_name + space + last_name
+    print(full_name)
+generate_name ()
+
+#Returning a value
+def generate_full_name ():
+    first_name = 'Jared'
+    last_name = 'Flomen'
+    space = ' '
+    full_name = first_name + space + last_name
+    return full_name
+print(generate_full_name())
