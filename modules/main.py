1e55217df8c661754f08acd8e49e84b59aeac4d6
Jared Flomen <jaredflomen@gmail.com>
2021-02-02 09:21:10 -0500

    Importing a function and renaming

--- modules/main.py
+++ modules/main.py
@@ -1,2 +1,3 @@
-import mymodules
-print(mymodules.generate_full_name('Jared', 'Flomen'))
+from mymodules import generate_full_name, sum_two_num as sum
+print(generate_full_name('Jared', 'Flomen'))
+print(sum(1, 2))
--- modules/mymodules.py
+++ modules/mymodules.py
@@ -2,3 +2,6 @@ def generate_full_name(firstname, lastname):
   space = ' '
   fullname = firstname + space + lastname
   return fullname
+
+def sum_two_num(num1, num2):
+  return num1 + num2
