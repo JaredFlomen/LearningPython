a7ebe84ee502001788a655122bcc4a5c7f6051a0
Jared Flomen <jaredflomen@gmail.com>
2021-02-03 11:01:18 -0500

    Create list_comprehension.py
    Creating a list from a sequence

---
+++ list_comprehension.py
@@ -0,0 +1,6 @@
+even_numbers = [i for i in range(21) if i % 2 == 0]
+print(even_numbers)
+
+numbers = [-8, -7, -3, -1, 0, 1, 3, 4, 5, 7, 6, 8, 10]
+positive__and_even = [i for i in numbers if i % 2 == 0 and i > 0]
+print(positive__and_even)
