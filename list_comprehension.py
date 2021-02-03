6c2eccf72da936a8fc9011bee8bb605fe7cb6d65
Jared Flomen <jaredflomen@gmail.com>
2021-02-03 11:03:33 -0500

    Update list_comprehension.py
    Lambda function

--- list_comprehension.py
+++ list_comprehension.py
@@ -4,3 +4,9 @@ print(even_numbers)
 numbers = [-8, -7, -3, -1, 0, 1, 3, 4, 5, 7, 6, 8, 10]
 positive__and_even = [i for i in numbers if i % 2 == 0 and i > 0]
 print(positive__and_even)
+
+#Lambda function
+add_numbers = lambda a, b: a + b
+print(add_numbers(1, 2))
+#Self invoking lambda
+print((lambda a, b: a + b)(2,3))
