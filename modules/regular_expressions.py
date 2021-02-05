089397b91cf1af050aad5595c9cd9e6491b12b81
Jared Flomen <jaredflomen@gmail.com>
2021-02-05 10:25:51 -0500

    Update regular_expressions.py
    Returning a list with findall

--- modules/regular_expressions.py
+++ modules/regular_expressions.py
@@ -1,7 +1,7 @@
 #A regular expression (Regex) helps find patterns in data
 import re
 
-text = '''Python is an awesome language to learn. It has many real world applications '''
+text = '''Python is an awesome language to learn. It has many real world applications. I love python'''
 
 match = re.search('awesome', text, re.I)
 print(match)
@@ -9,3 +9,6 @@ span = match.span()
 print(span)
 start, end = span
 print(text[start:end])
+
+matches = re.findall('python', text, re.I)
+print(matches)
