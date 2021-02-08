d7e1454f82bbec64e392c12056b1c4a8f45bab9f
Jared Flomen <jaredflomen@gmail.com>
2021-02-08 16:49:55 -0500

    Update objects.py
    Modifying a class's values with append

--- modules/objects.py
+++ modules/objects.py
@@ -5,11 +5,17 @@ class Person:
     self.age = age
     self.country = country
     self.city = city
+    self.skills = ["React", "Ruby on Rails"]
   
   def person_info(self):
     return f'{self.firstname} {self.lastname} lives in {self.city}'
+
+  def add_skill(self, skill):
+    self.skills.append(skill)
 
 p = Person('Jared', 'Flomen', 26, 'Canada', 'Toronto')
 print(p.firstname)
 print(p.city)
 print(p.person_info())
+p.add_skill("Python")
+print(p.skills)
