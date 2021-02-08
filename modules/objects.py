66078a03d6b8334d366652c37f7510c4e720d261
Jared Flomen <jaredflomen@gmail.com>
2021-02-08 16:45:26 -0500

    Update objects.py
    Expanding the class

--- modules/objects.py
+++ modules/objects.py
@@ -1,6 +1,11 @@
 class Person:
-  def __init__(self, name):
-    self.name = name
+  def __init__(self, firstname, lastname, age, country, city):
+    self.firstname = firstname
+    self.lastname = lastname
+    self.age = age
+    self.country = country
+    self.city = city
 
-p = Person('Jared')
-print(p.name)
+p = Person('Jared', 'Flomen', 26, 'Canada', 'Toronto')
+print(p.firstname)
+print(p.city)
