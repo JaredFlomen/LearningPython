e69613b6feaaf4e2801bd3ba5d5980ba1cfeb3ba
Jared Flomen <jaredflomen@gmail.com>
2021-02-10 10:15:24 -0500

    Inserting data

--- web/app.py
+++ web/app.py
@@ -1,19 +1,28 @@
 # let's import the flask
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, url_for
 import os # importing operating system module
 
 app = Flask(__name__)
 
 @app.route('/') # this decorator create the home route
 def home ():
-    return render_template('home.html')
+    techs = ['HTML', 'CSS', 'Flask', 'Python']
+    name = 'Jared Learning Python'
+    return render_template('home.html', techs=techs, name = name, title = 'Home')
 
 @app.route('/about')
 def about():
-    return render_template('about.html')
+    name = 'Jared Learning Python'
+    return render_template('about.html', name = name, title = 'About Us')
+
+@app.route('/post')
+def post():
+    name = 'Text Analyzer'
+    return render_template('post.html', name = name, title = name)
+
 
 if __name__ == '__main__':
-    # for deployment we use the environ
+    # for deployment
     # to make it work for both production and development
     port = int(os.environ.get("PORT", 5000))
     app.run(debug=True, host='0.0.0.0', port=port)
