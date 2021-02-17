016a8f9b53ebdc01dd25558f62c4b4e258c9785e
Jared Flomen <jaredflomen@gmail.com>
2021-02-17 16:04:09 -0500

    Copy with another object

--- advanced_python/copying.py
+++ advanced_python/copying.py
@@ -5,11 +5,22 @@ class Person:
     self.name = name
     self.age = age
 
+class Company:
+  def __init__(self, boss, employee):
+    self.boss = boss
+    self.employee = employee
+
 p1 = Person("Jared", 26)
-p2 = copy.copy(p1)
+p2 = Person('Adam', 20)
 
-p2.age = 28
+# p2.age = 28
 
-print(p2.age)
-print(p1.age)
+# print(p2.age)
+# print(p1.age)
 
+company = Company(p1, p2)
+company_clone = copy.copy(company)
+
+company_clone.boss.age = 27
+print(company_clone.boss.age)
+print(company.boss.age)
